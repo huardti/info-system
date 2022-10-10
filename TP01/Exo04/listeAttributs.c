8413e8071afef393b08a2f770f4f8b323a86b9aa
Robin SHAMSNEJAD <rsham@free.fr>
2022-10-10 17:15:41 +0200

    update exo04

--- TP01/Exo04/listeAttributs.c
+++ TP01/Exo04/listeAttributs.c
@@ -1,6 +1,7 @@
 #define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/stat.h>
 
 long unsigned int get_inode(char* path)
@@ -10,7 +11,7 @@ long unsigned int get_inode(char* path)
     if(!fd)
     {
         fprintf(stderr, "Unable to open file %s\n", path);
-        exit(EXIT_FAILURE);
+        return 0;
     }
 
     struct stat fileinfo = {0};
@@ -19,6 +20,14 @@ long unsigned int get_inode(char* path)
     fclose(fd);
 
     return fileinfo.st_ino;
+}
+int print_inode(char* path)
+{
+    long unsigned inode = get_inode(path);
+    if(inode == 0) return -1;
+
+    printf("Inode number of %s is %ld\n",path, get_inode(path));
+    return 0;
 }
 
 int main(int argc, char *argv[])
@@ -29,7 +38,44 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    printf("Inode number of %s is %ld\n", argv[1], get_inode(argv[1]));
+    char* path = argv[1];
+
+
+    FILE* fd = fopen(path, "w+");
+    fputs("blblbl", fd);
+    print_inode(path);
+
+    fclose(fd);
+    fd = fopen(path, "r");
+    char* line = NULL;
+    while(getline(&line, NULL, fd) != -1)
+    {
+        printf("%s", line);
+    }
+    print_inode(path);
+
+    fclose(fd);
+    print_inode(path);
+
+    printf("Press Enter :");
+    getchar();
+    print_inode(path);
+
+    fd = fopen(path, "a");
+    print_inode(path);
+
+    unlink(path);
+    print_inode(path);
+
+    fputs("blublu", fd);
+    print_inode(path);
+
+    getchar();
+    print_inode(path);
+
+    fclose(fd);
+    print_inode(path);
+
 
     return EXIT_SUCCESS;
 }
