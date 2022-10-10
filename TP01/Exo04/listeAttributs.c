55c5a0a0fd75d7849c446eb65a2006124e406605
Robin SHAMSNEJAD <rsham@free.fr>
2022-10-10 16:21:39 +0200

    Switch to CMake with exo04

---
+++ TP01/Exo04/listeAttributs.c
@@ -0,0 +1,28 @@
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+int main(int argc, char *argv[])
+{
+    if(argc != 2)
+    {
+        fprintf(stderr, "I need a filename !\n");
+        exit(EXIT_FAILURE);
+    }
+
+    FILE* fd = fopen(argv[1], "r");
+
+    if(!fd)
+    {
+        fprintf(stderr, "Unable to open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    struct stat fileinfo = {0};
+    fstat(fileno(fd), &fileinfo);
+
+    printf("Inode number is %ld\n", fileinfo.st_ino);
+
+    return EXIT_SUCCESS;
+}
