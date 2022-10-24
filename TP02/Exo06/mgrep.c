51b53124c098a974c6281137ea2c0230a74a5ccb
Robin SHAMSNEJAD <rsham@free.fr>
2022-10-24 17:30:28 +0200

    Add final question

--- TP02/Exo06/mgrep.c
+++ TP02/Exo06/mgrep.c
@@ -4,6 +4,8 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+#define MAXFILS 2
+
 int main(int argc, char* argv[])
 {
     if(argc < 3)
@@ -13,9 +15,16 @@ int main(int argc, char* argv[])
     }
 
     pid_t pid;
+    int fils = 0;
 
     for(int i = 2 ; i < argc ; ++i)
     {
+        if(fils >= MAXFILS)
+        {
+            printf("Attente : PID %d terminé.\n", wait(NULL));
+            --fils;
+        }
+
         pid = fork();
 
         if(pid < 0)
@@ -26,14 +35,18 @@ int main(int argc, char* argv[])
         else if(pid > 0)
         {
             printf("Grep PID %d lancé, en attente...\n", pid);
-            waitpid(pid, NULL, 0);
-            printf("Grep PID %d terminé.\n", pid);
+            ++fils;
         }
         else
         {
             execlp("grep", "grep", argv[1], argv[i], NULL);
             break;
         }
+    }
+
+    for(int i = 0 ; i < MAXFILS ; ++i)
+    {
+        printf("Grep PID %d terminé.\n", wait(NULL));
     }
 
     return EXIT_SUCCESS;
