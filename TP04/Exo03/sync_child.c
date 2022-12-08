c63347c5c09a72144e7c0d297bdda6c5b433c67c
Robin SHAMSNEJAD <rsham@free.fr>
2022-12-08 14:45:32 +0100

    Working Exo03

--- TP04/Exo03/sync_child.c
+++ TP04/Exo03/sync_child.c
@@ -2,24 +2,47 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include "sem.h"
+
+#define TRUE 1
+#define FALSE 0
 
 int main(void)
 {
+    SEMAPHORE sem = sem_create(FALSE, "/tmp/lol", 2);
+    sem_init(sem, (unsigned short[]){1});
+
+    P(sem);
     pid_t pid = fork();
 
     if(pid < 0)
     {
         perror("fork()");
-        exit(EXIT_FAILURE);
+        return EXIT_FAILURE;
     }
     else if(pid > 0) // Parent
     {
-        printf("Parent PID %d created child PID %d\n", getpid(), pid);
+        printf("[Parent] PID %d created child PID %d\n", getpid(), pid);
+
+        puts("[Parent] Waiting on child");
+        P(sem);
+
+        puts("[Parent] Exiting");
+        V(sem);
+
+        sem_delete(sem);
+        return EXIT_SUCCESS;
     }
     else // Child
     {
-        printf("Child PID %d created by parent PID %d\n", getpid(), getppid());
-    }
+        printf("[Child] PID %d created by parent PID %d\n", getpid(), getppid());
 
-    return EXIT_SUCCESS;
+        puts("[Child] Sleeping 3 seconds...");
+        sleep(3);
+
+        puts("[Child] Exiting");
+        V(sem);
+
+        return EXIT_SUCCESS;
+    }
 }
