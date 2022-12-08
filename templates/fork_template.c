9b55ab68167664265845f716a87de814b9dcfe02
Robin SHAMSNEJAD <rsham@free.fr>
2022-12-08 14:46:35 +0100

    Update fork template

--- templates/fork_template.c
+++ templates/fork_template.c
@@ -10,16 +10,17 @@ int main(void)
     if(pid < 0)
     {
         perror("fork()");
-        exit(EXIT_FAILURE);
+        return EXIT_FAILURE;
     }
     else if(pid > 0) // Parent
     {
         printf("Parent PID %d created child PID %d\n", getpid(), pid);
+        return EXIT_SUCCESS;
     }
     else // Child
     {
         printf("Child PID %d created by parent PID %d\n", getpid(), getppid());
+        return EXIT_SUCCESS;
     }
 
-    return EXIT_SUCCESS;
 }
